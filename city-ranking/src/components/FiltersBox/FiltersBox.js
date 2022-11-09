b5f6cbb6b7587303ad149b34526aa24d9112379e
Pedro Monteiro <pmapm@ua.pt>
2022-11-09 12:30:11 +0000

    add bar chart example

--- city-ranking/src/components/FiltersBox/FiltersBox.js
+++ city-ranking/src/components/FiltersBox/FiltersBox.js
@@ -1,36 +1,30 @@
 import { Dropdown, Container } from "@nextui-org/react";
 import React from "react";
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 import './FiltersBox.css';
 
 const FiltersBox = () => {
-    
-    const [selected, setSelected] = React.useState(new Set(["health_care"]));
 
-    const selectedValue = React.useMemo(
-      () => Array.from(selected).join("\r\n").replaceAll("_", " "),
-      [selected]
-    );
-    
+    const animatedComponents = makeAnimated();
+
+    const options = [
+        { value: 'health', label: 'Health Care' },
+        { value: 'criminal', label: 'Criminal Rate' },
+        { value: 'pollution', label: 'Pollution' }
+    ]
+
     return (
         <Container className='filters-box'>
-            <Dropdown className="side-bar-dropdown">
-                <Dropdown.Button flat color="default" css={{ tt: "capitalize" }} className="side-bar-dropdown">
-                    {selectedValue}
-                </Dropdown.Button>
-                <Dropdown.Menu
-                    aria-label="Multiple selection actions"
-                    color="secondary"
-                    disallowEmptySelection
-                    selectionMode="multiple"
-                    selectedKeys={selected}
-                    onSelectionChange={setSelected}
-                >
-                    <Dropdown.Item key="health_care">Health Care</Dropdown.Item>
-                    <Dropdown.Item key="pollution">Pollution</Dropdown.Item>
-                    <Dropdown.Item key="criminal">Criminal Rate</Dropdown.Item>
-                    <Dropdown.Item key="ranking">Ranking</Dropdown.Item>    
-                </Dropdown.Menu>
-            </Dropdown>
+            <Select
+                className="side-bar-dropdown"
+                closeMenuOnSelect={false}
+                components={animatedComponents}
+                defaultValue={[options[0]]}
+                isMulti
+                styles={{menuPortal: base => ({ ...base, zIndex: 9999 }) }}
+                options={options}
+            />
         </Container>
     )
 }
