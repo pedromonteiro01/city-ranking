e9afa2d891336fdfb35d97205a97e7dc83a05018
Pedro Monteiro <pmapm@ua.pt>
2022-11-21 13:13:44 +0000

    create dummy linechart

--- city-ranking/src/components/LineChart/LineChart.js
+++ city-ranking/src/components/LineChart/LineChart.js
@@ -1,8 +1,53 @@
 import { Dropdown } from "@nextui-org/react";
 import * as d3 from 'd3';
+import { scaleLinear } from "d3";
+import { useEffect, useRef, useState } from "react";
 import './LineChart.css';
 
 const LineChart = () => {
+    const [data, setData] = useState([35, 90, 205, 159, 94, 120])
+    const svgRef = useRef();
+
+    useEffect(() => {
+        const margin = { top: 50, right: 30, bottom: 30, left: 60 }
+        const w = parseInt(d3.select('#linechart').style('width')) - margin.left - margin.right
+        const h = parseInt(d3.select('#linechart').style('height')) - margin.top - margin.bottom
+        const svg = d3.select(svgRef.current)
+            .attr('width', w)
+            .attr('height', h)
+            .style('margin-top', '50')
+            .style('overflow', 'visible');
+
+        const xScale = d3.scaleLinear()
+            .domain([0, data.length - 1])
+            .range([0, w]);
+        const yScale = scaleLinear()
+            .domain([0, h])
+            .range([h, 0])
+        const generateScaledLine = d3.line()
+            .x((d,i) => xScale(i))
+            .y(yScale)
+            .curve(d3.curveCardinal);
+
+        const xAxis = d3.axisBottom(xScale)
+            .ticks(data.length)
+            .tickFormat(i => i + 1);   
+        const yAxis = d3.axisLeft(yScale)
+            .ticks(5);
+        svg.append('g')
+            .call(xAxis)
+            .attr('transform', `translate(0, ${h})`)
+        svg.append('g')
+            .call(yAxis) 
+
+        svg.selectAll('.line')
+            .data([data])
+            .join('path')
+            .attr('d', d => generateScaledLine(d))
+            .attr('fill', 'none')
+            .attr('stroke', 'black')
+    }, [data])
+
     const menuItems = [
         { key: "health", name: "Health Care" },
         { key: "criminal", name: "Criminal Rate" },
@@ -25,6 +70,11 @@ const LineChart = () => {
                         )}
                     </Dropdown.Menu>
                 </Dropdown>
+            </div>
+            <div className="line-chart-item">
+                <div id="linechart">
+                <svg ref={svgRef}></svg>
+                </div>
             </div>
         </div>
     )
