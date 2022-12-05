f969c39d4c1ac27bc00e02a81a5a7f3feeff1dd6
rezeett <edu_fernandes_2001@live.com.pt>
2022-12-05 01:49:09 +0000

    line chart changes

--- city-ranking/src/components/LineChart/LineChart.js
+++ city-ranking/src/components/LineChart/LineChart.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from "react";
 import { Dropdown } from "@nextui-org/react";
 import './LineChart.css';
 import * as d3 from "d3";
+import Modal from 'react-awesome-modal';
 import useResizeObserver from "./useResizeObserver";
 
 /**
@@ -11,11 +12,16 @@ import useResizeObserver from "./useResizeObserver";
 const ZoomableLineChart = (props) => {
     const svgRef = useRef();
     const wrapperRef = useRef();
+    const configRef = useRef();
     const dimensions = useResizeObserver(wrapperRef);
     const [data, setData] = useState(props.data);
-    const [city1, setCity1] = useState("Lisbon");
-    const [city2, setCity2] = useState("Porto");
-    const [city3, setCity3] = useState("Braga");
+    const [city, setCity] = useState("");
+
+    const [cities, setCities] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [showAutocomplete, setShowAutocomplete] = useState(false);
+    const [results, setResults] = useState();
 
     const menuItems = [
         { key: "health", name: "Health Care" },
@@ -25,30 +31,25 @@ const ZoomableLineChart = (props) => {
 
     var items = ["Cappuccino", "Cinema", "Wine", "Gasoline"]
 
+    console.log(data);
+
     // will be called initially and on every data change
     useEffect(() => {
-        SetupChart();
-    }, [data, dimensions]);
+        console.log("ola");
+        LoadChart();
+    }, [dimensions]);
 
-    const SetupChart = () => {
+    useEffect(()=>{
+        if (loading || !configRef.current) return;
+        SetupChart(data, cities);
+    }, [loading, cities])
+
+    const LoadChart = () => {
         const svg = d3.select(svgRef.current);
-        const svgContent = svg.select(".content");
+        const svgContent = svg.append("g").attr("class", "content");
         const { width, height } =
             dimensions || wrapperRef.current.getBoundingClientRect();
         //console.log("height: ", height)
-
-
-
-        var values = []
-        var keys = []
-        data.forEach((el) => {
-            if (el.city === city1 || el.city === city2 || el.city === city3){
-                keys.push({city: el.city, values: el.values})
-                for (let i in el.values){
-                    values.push(el.values[i].value);
-                }
-            }
-        })
 
         // scales + line generator
         var xScale = d3.scalePoint()
@@ -56,8 +57,39 @@ const ZoomableLineChart = (props) => {
             .domain(items);
 
         const yScale = d3.scaleLinear()
-            .domain([0, d3.max(values) + 1])
             .range([height - 10, 10]);
+
+        configRef.current = { svg, svgContent, xScale, yScale, height, width};
+        setLoading(false);
+    }
+
+    const SetupChart = (data, cities) => {
+        const { svg, svgContent, xScale, yScale, height, width} = configRef.current;
+
+        cities.forEach((city) => {
+            svg.selectAll("."+city).remove();
+            svg.selectAll("."+city+"legend").remove();
+            svg.selectAll(".pluslegend").remove();
+        })
+
+        var values = [];
+        var keys = [];
+        var len = [];
+        var j = 0;
+        cities.forEach((city) => {
+            data.forEach((el) => {
+                if (el.city===city){
+                    keys.push({city: el.city, values: el.values})
+                    len.push(j);
+                    for (let i in el.values){
+                        values.push(el.values[i].value);
+                    }
+                    j++;
+                }
+            })
+        })
+
+        yScale.domain([0, d3.max(values) + 1]);
 
         const lineGenerator = d3.line()
             .x(function (d) { return xScale(d.key) })
@@ -65,116 +97,145 @@ const ZoomableLineChart = (props) => {
             //.curve(curveCardinal);
 
         // A color scale: one color for each group
-        const myColor = d3.scaleOrdinal()
-        .domain(d3.range(3))
-        .range(d3.schemeSet2);
+
+        console.log(d3.schemeSet2);
+        keys.forEach((k) => {
+            console.log(k.city, d3.schemeSet2[0])
+        })
+
+        console.log(len);
 
         // render the line
         svgContent
             .selectAll(".myLine")
-            .data(keys)
+            .data(len)
             .join("path")
+            .transition()
+            .duration(400)
             .attr("class", "myLine")
-            .attr("stroke", d => myColor(d.city))
+            .attr("stroke", d => d3.schemeSet2[d])
             .attr("stroke-width", 4)
             .attr("fill", "none")
-            .attr("class", d => d.city)
-            .attr("d", d => lineGenerator(d.values));
+            .attr("class", d => keys[d].city)
+            .attr("d", d => lineGenerator(keys[d].values));
 
-            svg
+        svg
             // First we need to enter in a group
             .selectAll("myDots")
-            .data(keys)
+            .data(len)
             .join('g')
-              .style("fill", d => myColor(d.city))
-              .attr("class", d => d.city)
+              .style("fill", d => d3.schemeSet2[d])
+              .attr("class", d => keys[d].city)
             // Second we need to enter in the 'values' part of this group
             .selectAll("myPoints")
-            .data(d => d.values)
+            .data(d => keys[d].values)
             .join("circle")
               .attr("cx", d => xScale(d.key))
               .attr("cy", d => yScale(d.value))
               .attr("r", 5)
               .attr("stroke", "white")
 
-        svg
-            .selectAll("myLabels")
-            .data(keys)
-            .enter()
-            .append('g')
-            .append("text")
-            .attr("class", d => d.city)
-            // use this to append label to last element
-            .datum(function (d) { return { key: d.city, value: d.values[d.values.length - 1] }; }) // keep only the last value of each time series
-            .attr("transform", function (d) { return "translate(" + xScale(d.value.key) + "," + yScale(d.value.value) + ")"; }) // Put the text at the position of the last point
-            .attr("x", 15) // shift the text a bit more right
-            .attr("y", 15) // shift the text a bit more right
-            .text(d => d.key)
-            .attr("fill", d => myColor(d.key))
-            .style("font-size", 15)
+        keys.push({city: "plus", values: null})
+        len.push(len.length);
 
-        svg
-            .selectAll("myLegend")
-            .data(keys)
+        svg.selectAll("myLegend")
+            .data(len)
             .join('g')
               .append("text")
                 .attr('x', (d,i) => 30 + i*60)
-                .attr('y', 30)
-                .text(d => d.city)
-                .style("fill", d => myColor(d.city))
-                .style("font-size", 15)
+                .attr('y', 0)
+                .attr("class", d => keys[d].city + "legend")
+                .text(d => keys[d].city == "plus" ? "+" : keys[d].city)
+                .style("fill", d => keys[d].city == "plus" ? "#000" : d3.schemeSet2[d])
+                .style("font-size", "15px")
                 .style("cursor", "pointer")
               .on("click", function(event,d){
-                // is the element currently visible ?
-                let currentOpacity = d3.selectAll("." + d.city).style("opacity")
-                // Change the opacity: from 0 to 1 or from 1 to 0
-                d3.selectAll("." + d.city).transition().style("opacity", currentOpacity == 1 ? 0:1)})
-
+                    // is the element currently visible ?
+                    if (keys[d].city=="plus"){
+                        changeModal();
+                        //setCities(cities.concat("Paris"));
+                    }
+                    else {
+                        let currentOpacity = d3.selectAll("." + keys[d].city).style("opacity")
+                        // Change the opacity: from 0 to 1 or from 1 to 0
+                        d3.selectAll("." + keys[d].city).transition().style("opacity", currentOpacity == 1 ? 0:1)
+                    }
+                })
 
         // axes
         const xAxis = d3.axisBottom(xScale);
-        svg
-            .select(".x-axis")
+        svg.select(".x-axis")
             .attr("transform", `translate(0, ${height})`)
+            .style("font-size", "13px")
             .call(xAxis);
 
         const yAxis = d3.axisLeft(yScale);
-        svg.select(".y-axis").call(yAxis);
+        svg.select(".y-axis").transition().duration(1000).call(yAxis);
+
+        //d3.selectAll("g").remove();
+    }
+
+    const changeModal = () => {
+        setShowModal(!showModal);
+    }
+
+    const handleCitySearch = (event) => {
+        var str = event.target.value;
+        setCity(str);
+        str = str.toLowerCase();
+        var result = [];
+        data.forEach((d) => {
+            var lcCity = d.city.toLowerCase();
+            if (lcCity.includes(str)){
+                setShowAutocomplete(true);
+                result.push(<li key={lcCity} onClick={() => {setShowAutocomplete(false); changeModal(); setCity(""); setCities(cities.concat(d.city));}}>{lcCity}</li>)
+            }
+            setResults(result);
+        })
+
     }
 
     return (
-        <div className="line-chart animate__animated animate__fadeInDown">
-            <div className='line-chart-header'>
-                <h3>Products Price by City</h3>
-                <Dropdown>
-                    <Dropdown.Button flat>Trigger</Dropdown.Button>
-                    <Dropdown.Menu aria-label="Dynamic Actions" items={menuItems}>
-                        {(item) => (
-                            <Dropdown.Item
-                                key={item.key}
-                                color={item.key === "delete" ? "error" : "default"}
-                            >
-                                {item.name}
-                            </Dropdown.Item>
-                        )}
-                    </Dropdown.Menu>
-                </Dropdown>
-            </div>
-            <div className="line-chart-item">
-                <div ref={wrapperRef} style={{ marginBottom: "2rem", height: '30rem' }}>
-                    <svg ref={svgRef} className="svg">
-                        <defs>
-                            <clipPath>
-                                <rect x="0" y="0" width="100%" height="100%" />
-                            </clipPath>
-                        </defs>
-                        <g className="content"></g>
-                        <g className="x-axis" />
-                        <g className="y-axis" />
-                    </svg>
+        <>
+            <Modal visible={showModal} width="400" effect="fadeInDown" onClickAway={() => changeModal()}>
+                <div>
+                    <h1>Title</h1>
+                    <p>Some Contents</p>
+                    <input autoComplete="off" className='search-input' onChange={handleCitySearch} value={city} type="search" name="search" placeholder='Search city...' />
+                    <ul className='autocomplete-list'>
+                        {
+                            showAutocomplete && results
+                        }
+                    </ul>
+                </div>
+            </Modal>
+            <div className="line-chart animate__animated animate__fadeInDown">
+                <div className='line-chart-header'>
+                    <h3>Products Price by City</h3>
+                    <Dropdown>
+                        <Dropdown.Button flat>Trigger</Dropdown.Button>
+                        <Dropdown.Menu aria-label="Dynamic Actions" items={menuItems}>
+                            {(item) => (
+                                <Dropdown.Item
+                                    key={item.key}
+                                    color={item.key === "delete" ? "error" : "default"}
+                                >
+                                    {item.name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                <div className="line-chart-item">
+                    <div ref={wrapperRef} style={{ marginBottom: "2rem", height: '30rem' }}>
+                        <svg ref={svgRef} className="svg">
+                            <g className="x-axis"></g>
+                            <g className="y-axis"></g>
+                        </svg>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
